0aa6c1ac3bd71d6fca3df94da8fc93d73aeb9c48
clement.escande@3wa.io <clement.escande@3wa.io>
2022-03-29 21:35:01 +0200

    mdp change

--- src/main/java/com/epsi/fr/gosecuri/commun/Commun.java
+++ src/main/java/com/epsi/fr/gosecuri/commun/Commun.java
@@ -5,6 +5,6 @@ public class Commun {
 	public static final String cheminDossierRessource = "src/main/resources";
     public static final String cheminDossierGitGoSecuri = cheminDossierRessource + "/static/fichiersTest/git/GoSecuri-master";
     public static final String cheminDossierHtml = cheminDossierRessource + "/static/fichiersTest/html";
-	public static final String cheminDossierMdp = cheminDossierRessource + "/static/fichiersTest/mdp";
+	public static final String cheminDossierMdp = cheminDossierHtml + "/mdp";
 	
 }
